273bf33975a60f3d322fef1b4b83e2da534ca841
Kushagra-73 <kushagra.official.73@gmail.com>
2025-09-26 15:23:50 +0530

    main

--- Practical_9_Leap_Year.c
+++ Practical_9_Leap_Year.c
@@ -7,14 +7,13 @@ int main() {
     printf("Enter Year: ");
     scanf("%d", &year);
 
-    if (year > 999 && year < 10000) {
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            printf("%d is a leap year\n", year);
-        } else {
-            printf("%d is not a leap year\n", year);
-        }
-    } else {
-        printf("Enter a 4-digit year\n");
+    
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("%d is a leap year\n", year);
+    } 
+    else {
+        printf("%d is not a leap year\n", year);
     }
+    
     return 0;
 }
