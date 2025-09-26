bc6b3f56ab708f09d0eec680984bdfd6c132e69e
Kushagra-73 <kushagra.official.73@gmail.com>
2025-09-26 15:30:32 +0530

    main

--- Practical_1_Average_5_subjects.c
+++ Practical_1_Average_5_subjects.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 int main(){
-    int M1,M2,M3,M4,M5;
-    printf("Enter the Marks of M1, M2, M3, M4 and M5 respectively : ");
-    scanf("%d %d %d %d %d",&M1,&M2,&M3,&M4,&M5);
-    float avg = ((float) M1 + M2 + M3 + M4 + M5)/5;     // Doing typecasting as it is doing integer division
+    int Sub1,Sub2,Sub3,Sub4,Sub5;
+    printf("Enter the Marks of Sub1, Sub2, Sub3, Sub4 and Sub5 respectively : ");
+    scanf("%d %d %d %d %d",&Sub1,&Sub2,&Sub3,&Sub4,&Sub5);
+    float avg = ((float) Sub1 + Sub2 + Sub3 + Sub4 + Sub5)/5;     // Doing typecasting as it is doing integer division
     printf("The average value of marks of 5 subjects are : %.2f",avg);   //%.2f for making result in 2 decimal places
     return 0;
 }
